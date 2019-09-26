247c03b1a169637113f30dc6985546ba18244f07
薄阿战 <bofeilongaz@163.com>
2019-09-27 01:51:30 +0800

    读取数据库配置

--- Fas/Config.cs
+++ Fas/Config.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Net.Http;
+using System.Text.Json;
+using System.Xml;
 
 namespace Fas.Config
 {
     public class Config
     {
+        private NameValueCollection _conf;
         static Config()
         {
 
@@ -13,22 +18,61 @@ namespace Fas.Config
 
         private Config()
         {
-            string rootpath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data");
-            if (!File.Exists(Path.Combine(rootpath, "fas.conf"))) return;
+            string confPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "fas.conf");
+            if (!File.Exists(confPath)) return;
 
+            XmlDocument x = new XmlDocument();
+            x.Load(confPath);
+
+            XmlNode root = x.DocumentElement;
+
+            XmlNode node = root.SelectSingleNode("databases");
+            if (node == null || node.ChildNodes.Count == 0) return;
+            foreach (XmlNode item in node.ChildNodes)
+            {
+                if (item.Name == "add")
+                {
+                    string name = item.Attributes["name"]?.Value;
+                    if (string.IsNullOrEmpty(name)) continue;
+
+                    string connectionString = item.Attributes["connectionString"]?.Value;
+                    if (string.IsNullOrEmpty(connectionString)) continue;
+
+                    string provider = item.Attributes["providerName"]?.Value;
+                    if (string.IsNullOrEmpty(provider)) continue;
+
+                    _conf["databases.name"] = name;
+
+                    _conf["databases.provider"] = provider;
+
+                    _conf["databases.connectionString"] = connectionString;
+                }
+            }
+            /*
             string url = Environment.GetEnvironmentVariable("fas_conf_url");
-            if (string.IsNullOrEmpty(url)) return;
+            if (string.IsNullOrEmpty(url))
+            {
+                loadXml(confPath);
+                return;
+            }
+            string result = new HttpClient().GetAsync(url).Result.Content.ReadAsStringAsync().Result;
 
-            var result = new HttpClient().GetAsync(url);
+            List<Dictionary<string, string>> list = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(result);
 
+            string text = File.ReadAllText(confPath);
+            foreach (var item in list)
+            {
+                //text = text.Replace(,"");
+            }
 
+    */
         }
 
         public static Config Instance { get; } = new Config();
 
-        public void Load()
+        public string GetConfig(string key)
         {
-
+            return _conf[key];
         }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Fas.Config;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Test
@@ -9,19 +13,23 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
-        }
+            string result = "[{\"$key1\":\"value1\"},{\"$key2\":\"value2\"},{\"$key3\":\"value3\"}]";
 
-        /// <summary>
-        /// 异步方法
-        /// </summary>
-        /// <returns></returns>
-        static async Task AsyncTestMethod()
-        {
-            await Task.Run(() => {
-                    Config config = Config.Instance;
-                    config.Load();
-            });
+            string confPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "fas.config");
+            if (!File.Exists(confPath)) return;
+
+            List<Dictionary<string, string>> list = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(result);
+
+            string text = File.ReadAllText(confPath);
+            foreach (var item in list)
+            {
+                foreach (var key in item.Keys)
+                {
+                    text = text.Replace(key, item[key]);
+                }
+            }
+
+            Console.WriteLine();
         }
     }
 }
